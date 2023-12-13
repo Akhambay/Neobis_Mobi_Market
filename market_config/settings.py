dd533359dc70352b53feaeddda04714f9982763b
Akhambay <assyl.akhambay@gmail.com>
2023-12-14 01:42:25 +0600

    Add Swagger

--- market_config/settings.py
+++ market_config/settings.py
@@ -50,6 +50,7 @@ INSTALLED_APPS = [
     "allauth.account",
     "allauth.socialaccount",
     "dj_rest_auth.registration",
+    "drf_spectacular",
 
 ]
 
@@ -61,6 +62,13 @@ REST_FRAMEWORK = {
         "rest_framework.authentication.SessionAuthentication",
         "rest_framework.authentication.TokenAuthentication",
     ],
+    "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
+}
+
+SPECTACULAR_SETTINGS = {
+    "TITLE": "Mobi Market Project",
+    "DESCRIPTION": "A mobi market to learn about DRF",
+    "VERSION": "1.0.0",
 }
 
 AUTH_USER_MODEL = "accounts.CustomUser"
--- market_config/urls.py
+++ market_config/urls.py
@@ -16,10 +16,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("products.urls")),
     path("api/v1/auth/", include("dj_rest_auth.urls")),
     path("api/v1/auth/registration/", include("dj_rest_auth.registration.urls")),
+    path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
+    path("api/schema/swagger/",
+         SpectacularSwaggerView.as_view(url_name="schema"), name="swagger-ui"),
 ]
