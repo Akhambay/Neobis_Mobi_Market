551136b3c2ee37da9f44e9b2f0947805b469223a
Akhambay <assyl.akhambay@gmail.com>
2023-12-14 01:22:53 +0600

    Add Log In function

--- market_config/urls.py
+++ market_config/urls.py
@@ -20,4 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("products.urls")),
+    path("api/v1/auth/", include("dj_rest_auth.urls")),
 ]
