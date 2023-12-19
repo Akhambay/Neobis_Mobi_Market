7d7f7fd20828f285599b4e75773f273689e2cb2d
Akhambay <assyl.akhambay@gmail.com>
2023-12-19 21:19:10 +0600

    Update statics

--- market_config/settings.py
+++ market_config/settings.py
@@ -13,12 +13,12 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 from pathlib import Path
 import os
 
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
-# BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent
 
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
@@ -194,3 +194,6 @@ CLOUDINARY_STORAGE = {
     'API_KEY': "347283367819982",
     'API_SECRET': "uBGpubVL25VUdaEkCc_uxkRkCKE"
 }
+
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
