4eb7b4e9b651357be3ae115f5fd5eaedfc90c191
Akhambay <assyl.akhambay@gmail.com>
2023-12-25 14:51:13 +0600

    Update phone number confirmation

--- accounts/views.py
+++ accounts/views.py
@@ -17,18 +17,18 @@ from rest_framework.permissions import AllowAny
 def enter_verification_code(request):
     if request.method == 'POST':
         verification_code = request.data.get('verification_code')
-        email = request.data.get('email')
+        phone_number = request.data.get('phone_number')
 
         try:
             user = CustomUser.objects.get(
-                email=email, verification_code=verification_code)
+                phone_number=phone_number, verification_code=verification_code)
         except CustomUser.DoesNotExist:
-            return Response({'detail': 'Invalid verification code or email.'}, status=status.HTTP_400_BAD_REQUEST)
+            return Response({'detail': 'Invalid verification code or phone number.'}, status=status.HTTP_400_BAD_REQUEST)
 
         user.user_verified = True
         user.save()
 
-        return Response({'detail': 'Email verified successfully.'}, status=status.HTTP_200_OK)
+        return Response({'detail': 'Phone number verified successfully.'}, status=status.HTTP_200_OK)
 
 
 class CustomRegisterView(RegisterView):
@@ -43,8 +43,8 @@ class CustomRegisterView(RegisterView):
         user.verification_code = verification_code
         user.save()
 
-        subject = 'Verify Your Email. Project 8'
-        message = f'Your verification code is: {verification_code}'
+        subject = 'Welcome to Project 8'
+        message = 'This project is realized by awesome SAM team! Sardana, Assyl, Margulan'
         from_email = 'assyl.akhambay@gmail.com'
         recipient_list = user.email
 
@@ -63,8 +63,27 @@ class CustomUserDetailsView(generics.RetrieveUpdateAPIView):
 
     def perform_update(self, serializer):
         existing_profile_image = serializer.instance.profile_image
+        existing_phone_number = serializer.instance.phone_number
         serializer.save()
 
         if 'profile_image' not in self.request.data or not self.request.data['profile_image']:
             serializer.instance.profile_image = existing_profile_image
             serializer.instance.save()
+
+        new_phone_number = self.request.data.get('phone_number', None)
+        if new_phone_number != existing_phone_number:
+            serializer.instance.user_verified = False
+            serializer.instance.save()
+            verification_code = get_random_string(
+                length=4, allowed_chars='1234567890')
+            serializer.instance.verification_code = verification_code
+            serializer.instance.save()
+
+            subject = 'Verify Your Phone Number. Project 8'
+            message = f'Your verification code is: {verification_code}'
+            from_email = 'assyl.akhambay@gmail.com'
+            recipient_list = [serializer.instance.email]
+
+            send_mail(subject, message, from_email, recipient_list)
+
+        return Response({'detail': 'Profile updated successfully.'}, status=status.HTTP_200_OK)
