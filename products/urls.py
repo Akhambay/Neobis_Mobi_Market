c37ef4e6845f48731d09bee1933883312af10725
Akhambay <assyl.akhambay@gmail.com>
2023-12-26 15:14:36 +0600

    Add form-data usage

--- products/urls.py
+++ products/urls.py
@@ -9,5 +9,6 @@ urlpatterns = [
     path("products/<int:pk>/", ProductDetail.as_view(), name="product_detail"),
     path('products/my-list/', UserProductListView.as_view(), name='my-list'),
     path('products/add/', ProductCreateView.as_view(), name='create-product'),
+    path('api/products/create/', ProductCreateView.as_view(), name='product-create'),
     path('products/liked/', LikedProductListView.as_view(), name="products_liked"),
 ]
--- products/views.py
+++ products/views.py
@@ -1,11 +1,20 @@
-from rest_framework import generics, permissions
+from rest_framework import generics, permissions, status
+from rest_framework.response import Response
 from .models import Product
 from .serializers import ProductSerializer
 
 
 class ProductCreateView(generics.CreateAPIView):
+    queryset = Product.objects.all()
     serializer_class = ProductSerializer
     permission_classes = [permissions.IsAuthenticated]
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
