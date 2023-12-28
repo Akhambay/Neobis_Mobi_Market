e2681fb44e20c221b67597334498412904ed0c88
Akhambay <assyl.akhambay@gmail.com>
2023-12-29 01:48:40 +0600

    Update Products urls.py

--- products/urls.py
+++ products/urls.py
@@ -6,10 +6,10 @@ router = DefaultRouter()
 router.register(r'products', ProductViewSet, basename='product')
 
 urlpatterns = [
-    path('', include(router.urls)),
     path("products/all", ProductList.as_view(), name="product_list"),
     path("products/<int:pk>/", ProductDetail.as_view(), name="product_detail"),
     path('products/my-list/', UserProductListView.as_view(), name='my-list'),
     path('products/add/', ProductCreateView.as_view(), name='create-product'),
     path('products/liked/', LikedProductListView.as_view(), name="products_liked"),
+    path('', include(router.urls)),
 ]
